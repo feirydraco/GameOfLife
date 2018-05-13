9c87b68222a63b84f6a4fb0a164246b37bdc0f9c
Yash Vora <majinsparks@gmail.com>
2018-05-13 16:09:54 +0530

    .

--- CLIgol.py
+++ CLIgol.py
@@ -1,93 +1,77 @@
 
 from random import randint
-board_height = 10
-board_width = 60
-board = []
-temp_board = []
 
-def board_init(board):
-    for i in range(0, (board_height + 1)):
-        board.append([0] * (board_width + 1))
+class GOL:
+    def __init__(self, data):
+        self.board_height = 100
+        self.board_width = 100
+        self.board = []
+        self.temp_board = []
+        self.data = data
+        self.n = 0
+        for char in data:
+            self.n += ord(char)
+    
+    def board_init(self, board):
+        for i in range(0, (self.board_height + 1)):
+            self.board.append([0] * (self.board_width + 1))
+    
+#    def print_board_beta(board):
+#        for i in range(board_height):
+#            for j in range(board_width):
+#                print(board[i][j], end = " ")
+#            print(sep = "\n")
+            
+    def add_org(self, x, y):
+        self.board[x][y] = 1
+        
+    def copy_board(source, dest):
+        for i in range(0, board_height):
+            for j in range(0, board_width):
+                dest[i][j] = source[i][j]
+    
+    def nbrs(self, x, y):
+        no = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                #print i, j
+                if self.board[x + i][y + j] == 1 and ((x + i >= 0 and y + j >= 0) and (x + i < self.board_height and y + j < self.board_width)):
+                    no = no + 1
+        if self.board[x][y] == 1:
+            no = no - 1
+        return no
+    
+    def play(self, board):
+        board_init(self.temp_board)
+        for i in range(0, self.board_height - 1):
+            for j in range(0, self.board_width - 1):
+                if nbrs(board, i, j) < 2 or nbrs(board, i, j) > 3:
+                    self.temp_board[i][j] = 0
+                elif (nbrs(board, i, j) == 2 or nbrs(board, i, j) == 3) and board[i][j] == 1:
+                    self.temp_board[i][j] = 1
+                elif nbrs(board, i, j) == 3 and board[i][j] == 0:
+                    self.temp_board[i][j] = 1
+    
+    board_init(self.board)
+    
+    print_board_beta(self.board)
+    
+    populate(board, n)
+    
+    print_board_beta(self.board)
+    print ("\n\n")
+    
+    
+    
+    for i in range(0, n):
+        print (("Generation"), i + 1)
+        print ("\n")
+        play(board)
+        print_board_beta(temp_board)
+        board_init(board)
+        copy_board(temp_board, board)
+        board_init(temp_board)
+        print ("\n")
 
-def print_board_beta(board):
-    for i in range(board_height):
-        for j in range(board_width):
-            print(board[i][j], end = " ")
-        print(sep = "\n")
-def add_org(board, x, y):
-    board[x][y] = 1
 
-
-def populate(board, n):
-    '''for i in range(0, board_height):
-        for j in range(0, board_width):
-            if board[i][j] != 1 and n > 0:
-                add_org(board, i, j)
-                n = n - 1'''
-    for q in range(0, n):
-        flag = 0
-        while flag != 1:
-            i = randint(0, board_height - 1)
-            j = randint(0, board_width - 1)
-
-            if board[i][j] != 1:
-                add_org(board, i, j)
-                flag = 1
-
-def copy_board(source, dest):
-    for i in range(0, board_height):
-        for j in range(0, board_width):
-            dest[i][j] = source[i][j]
-
-def nbrs(board, x, y):
-    no = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            #print i, j
-            if board[x + i][y + j] == 1 and ((x + i >= 0 and y + j >= 0) and (x + i < board_height and y + j < board_width)):
-                no = no + 1
-    if board[x][y] == 1:
-        no = no - 1
-    return no
-
-def play(board):
-    board_init(temp_board)
-    for i in range(0, board_height - 1):
-        for j in range(0, board_width - 1):
-            if nbrs(board, i, j) < 2 or nbrs(board, i, j) > 3:
-                temp_board[i][j] = 0
-            elif (nbrs(board, i, j) == 2 or nbrs(board, i, j) == 3) and board[i][j] == 1:
-                temp_board[i][j] = 1
-            elif nbrs(board, i, j) == 3 and board[i][j] == 0:
-                temp_board[i][j] = 1
-
-board_init(board)
-
-print_board_beta(board)
-
-cc = 0
-
-while(cc == 0):
-    n = int(input("Enter number of organisms to spawn: "))
-    if n <= board_height * board_width:
-        cc = 1
-    else:
-        print ("Too many organisms")
-
-
-populate(board, n)
-
-print_board_beta(board)
-print ("\n\n")
-
-gen = int(input("Simulate for how many generations?: "))
-
-for i in range(0, gen):
-    print (("Generation"), i + 1)
-    print ("\n")
-    play(board)
-    print_board_beta(temp_board)
-    board_init(board)
-    copy_board(temp_board, board)
-    board_init(temp_board)
-    print ("\n")
+g = GOL("hello")
--- convert_string_to_data.py
+++ convert_string_to_data.py
@@ -1,7 +1,21 @@
-import binascii
-str = "name"
-binaryarr = []
-for char in str:
-    binaryarr.append(bin(ord(char))[2:])
+x = "some string"
 
-print(binaryarr)
+n = 0
+
+for char in x:
+    n += ord(char)
+print(n)
+
+b_a = []
+
+for _ in x:
+    b_a.append(bin(ord(_)))
+
+actual_data = []
+
+for _ in b_a:
+    actual_data.append(int(_[2:]))
+
+
+
+
